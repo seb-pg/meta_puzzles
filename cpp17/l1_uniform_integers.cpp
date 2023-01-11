bdf9b11b2ae25e8b8f0cd69999b607408a6c8f0c
Sebastien Rubens <90621822+seb-pg@users.noreply.github.com>
2023-01-11 07:38:46 +0000

    Update C++17 code

--- cpp17/l1_uniform_integers.cpp
+++ cpp17/l1_uniform_integers.cpp
@@ -4,8 +4,8 @@
 // for more information
 //
 // To the extent possible under law, the person who associated CC0 with
-// openmsg has waived all copyright and related or neighboring rights
-// to openmsg.
+// meta_puzzles has waived all copyright and related or neighboring rights
+// to meta_puzzles.
 //
 // You should have received a copy of the CC0 legalcode along with this
 // work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
