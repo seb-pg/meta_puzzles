67068aa7bb0db527e4631e4d1f8df86ab998bbf8
Sebastien Rubens <90621822+seb-pg@users.noreply.github.com>
2023-01-13 07:46:41 +0000

    Update Go code

---
+++ go/main.go
@@ -0,0 +1,17 @@
+// meta_puzzles by Sebastien Rubens
+//
+// Please go to https://github.com/seb-pg/meta_puzzles/README.md
+// for more information
+//
+// To the extent possible under law, the person who associated CC0 with
+// meta_puzzles has waived all copyright and related or neighboring rights
+// to meta_puzzles.
+//
+// You should have received a copy of the CC0 legalcode along with this
+// work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+package main
+
+func main() {
+	// Yes, they are coming!
+}
